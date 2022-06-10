0184b87a12dde4a75a09453e7fadac6e6053c39b
Rockheung <rockheung@gmail.com>
2022-06-10 21:01:33 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,6 @@ const main = async () => {
   if ($app === null) {
     throw new Error("No app");
   }
-
-  const $header = document.createElement("header");
-  const $searchInput = document.createElement("input");
-  $searchInput.type = "text";
-  const $form = document.createElement("form");
-  $form.appendChild($searchInput);
-  $header.appendChild($form);
-  const $main = document.createElement("main");
-
-  $app.appendChild($header);
-  $app.appendChild($main);
 };
 
 main().catch(console.error);
